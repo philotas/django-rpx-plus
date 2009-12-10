97c2391e1940e6f355a298e70a40720434edefa5
Michael Huynh <mike.huynh@gmail.com>
2009-12-10 15:19:31 -0500

    Added urls.py

---
+++ django_rpx/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+#import django_rpx.views
+
+urlpatterns = patterns('django_rpx.views',
+    #url(r'^activate/complete/$',
+    #    direct_to_template,
+    #    { 'template': 'registration/activation_complete.html' },
+    #    name='registration_activation_complete'),
+    url(r'^rpx_response/$', 'rpx_response', name='rpx_response'),
+    #url(r'^register/$', 'register', name='auth_register'),
+    #url(r'^login/$', 'login', name='auth_login'),
+    #url(r'^logout/$', 'logout', name='auth_logout'),
+)

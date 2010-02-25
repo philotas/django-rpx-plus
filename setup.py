36b972fec47c3dd3f3e6928b1e2bfa7a338acebd
Michael Huynh <mike.huynh@gmail.com>
2010-02-25 12:30:57 -0500

    Moved django_rpx/ in a src/ dir and updated setup.py. These changes keep example/ app from being installed when using 'setup.py develop'.

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ setup(
     name = 'django_rpx',
     #We exclude example app from installing since that may interfere from
     #someone testing out their own example app of the same name. I got 
-    #bit by this :).
-    packages = find_packages(exclude = ['example', 'example.*']),
+    #bit by this :). We also put django_rpx/ inside of a src/ dir so that
+    #installations using 'setup.py develop' don't install example/ (since
+    #'setup.py develop' ignores exclude).
+    packages = find_packages('src', exclude = ['example', 'example.*']),
+    package_dir={'' : 'src'},
     version = '1.0.0',
     description  = 'RPX auth support for django',
     author='Michael Huynh',

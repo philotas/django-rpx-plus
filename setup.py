b09c07877695a03b681eb0c66d54377178d70665
Michael Huynh <mike.huynh@gmail.com>
2010-02-25 11:52:06 -0500

    Updating setup.py to exclude installation of example/ app.

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django_rpx',
-    packages = find_packages(),
+    #We exclude example app from installing since that may interfere from
+    #someone testing out their own example app of the same name. I got 
+    #bit by this :).
+    packages = find_packages(exclude = ['example', 'example.*']),
     version = '1.0.0',
     description  = 'RPX auth support for django',
     author='Michael Huynh',
